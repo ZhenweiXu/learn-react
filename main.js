2ebf4e3c1ddfa6ea5cd2d9c9ba2b9b15e11f6a93
jenvyxu <1272117264@qq.com>
2020-06-14 01:01:22 +0800

    perf: 使用react实现

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
-let result = document.querySelector('#result')
-let add = document.querySelector('#add')
-let minus = document.querySelector('#minus')
+// let result = document.querySelector('#result')
+// let add = document.querySelector('#add')
+// let minus = document.querySelector('#minus')
 
-add.addEventListener('click', function() {
-  let number =parseInt(result.innerText, 10)
+// add.addEventListener('click', function() {
+//   let number =parseInt(result.innerText, 10)
+//   number += 1
+//   result.innerText = number
+// })
+
+// minus.addEventListener('click', function() {
+//   let number =parseInt(result.innerText, 10)
+//   number -= 1
+//   result.innerText = number
+// })
+
+let number = 0
+let onClickButton1 = () => {
   number += 1
-  result.innerText = number
-})
-
-minus.addEventListener('click', function() {
-  let number =parseInt(result.innerText, 10)
+  render()
+}
+let onClickButton2 = () => {
   number -= 1
-  result.innerText = number
-})
+  render()
+}
+
+render()
+
+function render() {
+  let span = React.createElement('span', { className: 'red' }, number)
+  let button1 = React.createElement('button', { onClick: onClickButton1 }, '+')
+  let button2 = React.createElement('button', { onClick: onClickButton2 }, '-')
+  let div = React.createElement(
+    'div', { className: 'parent' }, span, button1, button2
+  )
+  ReactDOM.render(div, document.querySelector('#root'))
+}
+
