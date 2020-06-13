4d691fc3b81542d4d09ed84cd22c0ad6b221808f
jenvyxu <1272117264@qq.com>
2020-06-14 00:38:18 +0800

    feat: 实现一个加减功能的页面

---
+++ main.js
@@ -0,0 +1,15 @@
+let result = document.querySelector('#result')
+let add = document.querySelector('#add')
+let minus = document.querySelector('#minus')
+
+add.addEventListener('click', function() {
+  let number =parseInt(result.innerText, 10)
+  number += 1
+  result.innerText = number
+})
+
+minus.addEventListener('click', function() {
+  let number =parseInt(result.innerText, 10)
+  number -= 1
+  result.innerText = number
+})
