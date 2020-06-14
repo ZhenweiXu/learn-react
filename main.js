7958678e7d8a2906042deb1faac03ef65779f2f1
jenvyxu <1272117264@qq.com>
2020-06-14 10:09:21 +0800

    添加box组件

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-let number = 0
+let number1 = 0
+let number2 = 0
 let onClickButton1 = () => {
-  number += 1
+  number1 += 1
   render()
 }
 let onClickButton2 = () => {
-  number -= 1
+  number1 -= 1
+  render()
+}
+
+let onClickButton3 = () => {
+  number2 += 2
+  render()
+}
+
+let onClickButton4 = () => {
+  number2 -= 2
   render()
 }
 
@@ -13,28 +24,32 @@ render()
 function App() {
   return (
     <div>
-      <Box1/>
-      <Box2/>
+      <Box1 name="加一"/>
+      <Box2 name="加二"/>
     </div>
   )
 }
 
-function Box1() {
+function Box1(obj) {
   return (
     <div className='parent'>
-      <span className='red'>{number}</span>
-      <button onClick={onClickButton1}>+</button>
-      <button onClick={onClickButton2}>-</button>
+      我的name是{obj.name}
+      <hr/>
+      <span className='red'>{number1}</span>
+      <button onClick={onClickButton1}>+1</button>
+      <button onClick={onClickButton2}>-1</button>
     </div>
   )
 }
 
-function Box2() {
+function Box2(obj) {
   return (
     <div className='parent'>
-      <span className='red'>{number}</span>
-      <button onClick={onClickButton1}>+</button>
-      <button onClick={onClickButton2}>-</button>
+      我的name是{obj.name}
+      <hr/>
+      <span className='red'>{number2}</span>
+      <button onClick={onClickButton3}>+2</button>
+      <button onClick={onClickButton4}>-2</button>
     </div>
   )
 }
